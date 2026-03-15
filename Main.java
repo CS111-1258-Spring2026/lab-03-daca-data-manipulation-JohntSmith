ecba0a61ef34724206fbafd28b278c750291f294
JohntSmith <jonathan.tsmith14@gmail.com>
2026-03-15 23:03:26 +0000

    DACA + Data Manipulation JDN EXERCISE

--- Main.java
+++ Main.java
@@ -1,59 +1,44 @@
 // STUDENT NAME:Jonathan Smith
-// DATE: 2/11/2026
+// DATE: 3/15/2026
 
-public class Main 
-{
-  public static void main(String[] args) 
-	{
-
-		/***** DECLARATION SECTION *****/
-		int a, m, y, a1, m1, y1;
-		double jdnBirth, jdnToday;
-		int day, month, year,dayBirth, monthBirth, yearBirth;
-		double dayDiff,age;
-
-		/***** INITIALIZATION SECTION *****/
-		
+public class Main {
+    public static void main(String[] args) {
+/****** DECLARATION SECTION ******/
+    int a, m, y, a1, m1, y1;
+    int day, month, year, dayBirth, monthBirth, yearBirth;
+    double jdnBirth, jdnToday, dayDiff; 
+    double age;
 
 
+    /****** INITIALIZATION SECTION ******/
+    dayBirth = 26;
+    monthBirth = 8;
+    yearBirth = 1918;
 
-		/***** INPUT SECTION *****/
-		// N/A (no input for this lab)
-		
-		
-		
-		
-		/***** CALCULATION & PROCESSING SECTION *****/
-		// 1. Calculation for Birthdate.
-		dayBirth = 26;
-		monthBirth = 8;
-		yearBirth = 1918;
+    day = 2;
+    month = 2;
+    year = 2020;
 
-		a = (14 - monthBirth) / 12;
-		y = yearBirth + 4800 - a;
-		m = monthBirth + (12 * a) - 3;
-		jdnBirth = dayBirth + ((153 * m + 2) / 5) + (365 * y) + (y / 4) - (y / 100) + (y / 400) - 32045;
+    /****** CALCULATION & PROCESSING SECTION ******/
+    // Calculate JDN for Birthday
+    a = (14 - monthBirth) / 12;
+    y = yearBirth + 4800 - a;
+    m = monthBirth + (12 * a) - 3;
+    jdnBirth = dayBirth + ((153 * m + 2) / 5) + (365 * y) + (y / 4) - (y / 100) + (y / 400) - 32045;
 
-		// 2. Calculation for Today.
-		day = 2;
-		month = 2;
-		year = 2020;
+    // Calculate JDN for Today
+    a1 = (14 - month) / 12;
+    y1 = year + 4800 - a1;
+    m1 = month + (12 * a1) - 3;
+    jdnToday = day + ((153 * m1 + 2) / 5) + (365 * y1) + (y1 / 4) - (y1 / 100) + (y1 / 400) - 32045;
 
-		a1 = (14 - 2) / 12;
-		y1 = year + 4800 - a1;
-		m1 = month + (12 * a1) - 3;
-		jdnToday = day + ((153 * m1 + 2) / 5) + (365 * y1) + (y1 / 4) - (y1 / 100) + (y1 / 400) - 32045;
+    dayDiff = jdnToday - jdnBirth;
+    age = dayDiff / 365;
 
-	
-		dayDiff = jdnToday - jdnBirth;
-		age = dayDiff / 365;
-		
-		
-		/***** OUTPUT SECTION *****/
-		System.out.println( "Julian Day Number for today's date, 2/2/2020, is " + (int) jdnToday);
-		System.out.println( "Julian Day Number for birthday, 8/26/1918, is " + (int) jdnBirth);
-		System.out.print( "The difference in days is " + (int) (jdnToday - jdnBirth));
-		System.out.print( ", which makes you approximately " + (int) age);
-		System.out.println(" years old!");
-  }
+/****** OUTPUT SECTION ******/
+System.out.println("\nJulian Day Number for today's date, 2/2/2020, is " + (int)jdnToday);
+System.out.println("\nJulian Day Number for birthday, 8/26/1918, is " + (int)jdnBirth);
+System.out.println("\nThe difference in days is " + (int)dayDiff + ", which makes you approximately " + (int)age + " years old!");
+System.out.println();
+    }
 }
